5b833147e33ba1c5f59fa886eced03087ad75c45
FSitnikov1 <mr.prizrek@mail.ru>
2022-01-22 21:40:22 +0300

    Some HTML and CSS

--- WebProject/public/src/Components/LocSt.js
+++ WebProject/public/src/Components/LocSt.js
@@ -1,3 +1,4 @@
+import '../css/locst.css'
 import st1 from '../img/st1.svg'
 import st2 from '../img/st2.svg'
 import st3 from '../img/st3.svg'
@@ -10,6 +11,66 @@ import st8 from '../img/st8.svg'
 export default function Compet() {
     return (
         <section className="st">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h2>14 лет совершенствуем <br /> компетенции в Drupal <br /> поддержке!</h2>
+                        <div className="st-main-text">
+                            Разрабатываем и оптимизируем модули, расширяем <br /> функциональность сайтов, обновляем дизайн
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6 col-md-3 sto-block">
+                        <div className="sty-icon"><img src={st1} /></div>
+                        <div className="text">
+                            Добавление <br /> информации на сайт,<br /> создание новых <br /> разделов
+                        </div>
+                    </div>
+                    <div className="col-6 col-md-3 sto-block">
+                        <div className="sty-icon"><img src={st2} /></div>
+                        <div className="text">
+                            Разработка <br /> и оптимизация <br /> модулей сайта
+                        </div>
+                    </div>
+                    <div className="col-6 col-md-3 sto-block">
+                        <div className="sty-icon"><img src={st3} /></div>
+                        <div className="text">
+                            Интеграция с CRM, <br /> 1C, платежными <br /> системами, любыми <br /> веб-сервисами
+                        </div>
+                    </div>
+                    <div className="col-6 col-md-3 sto-block">
+                        <div className="sty-icon"><img src={st4} /></div>
+                        <div className="text">
+                            Любые доработки <br /> функционала <br /> и дизайна
+                        </div>
+                    </div>
+                    <div className="col-6 col-md-3 sto-block">
+                        <div className="sty-icon"><img src={st5} /></div>
+                        <div className="text">
+                            Аудит и мониторинг <br /> безопасности Drupal <br /> сайтов
+                        </div>
+                    </div>
+                    <div className="col-6 col-md-3 sto-block">
+                        <div className="sty-icon"><img src={st6} /></div>
+                        <div className="text">
+                            Миграция, импорт <br /> контента и апгрейд <br /> Drupal
+                        </div>
+                    </div>
+                    <div className="col-6 col-md-3 sto-block">
+                        <div className="sty-icon"><img src={st7} /></div>
+                        <div className="text">
+                            Оптимизация <br /> и ускорение <br /> Drupal-сайтов
+                        </div>
+                    </div>
+                    <div className="col-6 col-md-3 sto-block">
+                        <div className="sty-icon"><img src={st8} /></div>
+                        <div className="text">
+                            Веб-маркетинг, <br /> консультации <br /> и работы по SEO
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
--- WebProject/public/src/Components/Sup.js
+++ WebProject/public/src/Components/Sup.js
@@ -1,3 +1,4 @@
+import '../css/sup.css'
 import sup1 from '../img/sup1.svg'
 import sup2 from '../img/sup2.svg'
 import sup3 from '../img/sup3.svg'
@@ -10,6 +11,92 @@ import sup8 from '../img/sup8.svg'
 export default function Support() {
   return (
     <section className="sup">
+      <h2>Разработка<br />от Drupal-coder</h2>
+      <div className="container">
+        <div className="row mx-0">
+          <div className="col-12 col-md-6 col-lg-3 sup-col">
+            <div className="sup-cards">
+              <div className="sup-number">01.</div>
+              <div className="sup-headline">Постановка задачи по Email</div>
+              <div className="text">
+                Удобная и привычная модель постановки задач, при которой задачи фиксируются и никогда не теряются.
+              </div>
+              <div className="sup-img"><img src={sup1} /></div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 sup-col">
+            <div className="sup-cards">
+              <div className="sup-number">02.</div>
+              <div className="sup-headline">Система Helpdesk – отчетность, прозрачность</div>
+              <div className="text">
+                Возможность посмотреть все заявки в работе и отработанные часы в личном кабинете через браузер.
+              </div>
+              <div className="sup-img"><img src={sup2} /></div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 sup-col">
+            <div className="sup-cards">
+              <div className="sup-number">03.</div>
+              <div className="sup-headline">Расширенная техническая поддержка</div>
+              <div className="text">
+                Возможность организации расширенной техподдержки с 8:00 до 22:00 без выходных.
+              </div>
+              <div className="sup-img"><img src={sup3} /></div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 sup-col">
+            <div className="sup-cards">
+              <div className="sup-number">04.</div>
+              <div className="sup-headline">Персональный менеджер проекта</div>
+              <div className="text">
+                Ваш менеджер проекта всегда в курсе текущего состояния проекта и в любой момент готов ответить на любые
+                вопросы.
+              </div>
+              <div className="sup-img"><img src={sup4} /></div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 sup-col">
+            <div className="sup-cards">
+              <div className="sup-number">05.</div>
+              <div className="sup-headline">Удобные способы оплаты</div>
+              <div className="text">
+                Безналичный расчет по договору или электронные деньги: WebMoney, Яндекс.Деньги, Paypal.
+              </div>
+              <div className="sup-img"><img src={sup5} /></div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 sup-col">
+            <div className="sup-cards">
+              <div className="sup-number">06.</div>
+              <div className="sup-headline">Работаем с SLA и NDA</div>
+              <div className="text">
+                Работа в рамках соглашений о конфиденциальности и об уровне качества работ.
+              </div>
+              <div className="support-img"><img src={sup6} /></div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 sup-col">
+            <div className="sup-cards">
+              <div className="sup-number">07.</div>
+              <div className="sup-headline">Штатные специалисты</div>
+              <div className="text">
+                Надежные штатные специалисты, никаких фрилансеров.
+              </div>
+              <div className="sup-img"><img src={sup7} /></div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 sup-col">
+            <div className="sup-cards">
+              <div className="sup-number">08.</div>
+              <div className="sup-headline">Удобные каналы связи</div>
+              <div className="text">
+                Консультации по телефону, скайпу, в месенджерах.
+              </div>
+              <div className="sup-img"><img src={sup8} /></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
